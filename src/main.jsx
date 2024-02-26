07fed5033e979284aa3534610f88ddab471b3ce7
mariolink <sawtoise12345@gmail.com>
2024-02-26 19:21:00 +0000

    expirementing with dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createNightowl } from '@bufferhead/nightowl'
+
+createNightowl({
+    defaultMode: 'dark',
+    toggleButtonMode: 'newState'
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
