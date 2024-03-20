b54f2dbfde3b93e439f5261ed5c194858e6db98b
DDQuin <ddquin420@gmail.com>
2024-03-20 14:06:54 +0000

    Pass in setlatest photo so can update photo on capture

--- src/components/Main/LatestPhoto.jsx
+++ src/components/Main/LatestPhoto.jsx
@@ -4,61 +4,88 @@ import InfoIcon from "../../assets/Info.svg";
 import CameraIcon from "../../assets/Camera.svg";
 import "./LatestPhoto.css";
 import paramatersService from "../../services/parameters";
-import { Alert, Snackbar } from '@mui/material'
-import parameterService from '../../services/parameters.js'
+import { Alert, Snackbar } from "@mui/material";
+import parameterService from "../../services/parameters.js";
 
-const handleClick = async (setLoading, setErrorMessage, setOpenError, setOpenSuccess) => {
-    let data
-    let response
+const handleClick = async (
+  setLoading,
+  setErrorMessage,
+  setOpenError,
+  setOpenSuccess,
+  setLatestPhoto
+) => {
+  let data;
+  let response;
   try {
     setLoading(true);
-     response = await fetch(`${paramatersService.BASE_URL}/take_photo`);
-     data = await response.json()
+    response = await fetch(`${paramatersService.BASE_URL}/take_photo`);
+    data = await response.json();
     setLoading(false);
-     if (!response.ok) {
-         setErrorMessage(`Error ${response.status}: ${parameterService.getErrorMessage(response, data)}`)
-         throw new Error(`${response.status} ${parameterService.getErrorMessage(response, data)}`)
-     }
-      setOpenSuccess(true)
+    if (!response.ok) {
+      setErrorMessage(
+        `Error ${response.status}: ${parameterService.getErrorMessage(
+          response,
+          data
+        )}`
+      );
+      throw new Error(
+        `${response.status} ${parameterService.getErrorMessage(response, data)}`
+      );
+    }
+    setLatestPhoto(data);
+    setOpenSuccess(true);
   } catch (err) {
-      setOpenError(true)
-      setLoading(false);
+    setOpenError(true);
+    setLoading(false);
     console.log(err.message);
   }
 };
 
-
-
-
-const LatestPhoto = ({ latestPhoto }) => {
+const LatestPhoto = ({ latestPhoto, setLatestPhoto }) => {
   const [isLoading, setLoading] = useState(false);
-    const [openSuccess, setOpenSuccess] = useState(false)
-    const [openError, setOpenError] = useState(false)
-    const [errorMessage, setErrorMessage] = useState()
+  const [openSuccess, setOpenSuccess] = useState(false);
+  const [openError, setOpenError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
 
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return
-        }
-
-        setOpenSuccess(false)
-        setOpenError(false)
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
     }
+
+    setOpenSuccess(false);
+    setOpenError(false);
+  };
 
   return (
     <div className={"outerContainer"}>
-        <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={openSuccess}
-                  autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="success" sx={{ width: '100%', fontSize: 20 }}>
-                Success
-            </Alert>
-        </Snackbar>
-        <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={openError} autoHideDuration={6000}
-                  onClose={handleClose}>
-            <Alert onClose={handleClose} severity="error" sx={{ width: '100%', fontSize: 20 }}>
-                {errorMessage}
-            </Alert>
-        </Snackbar>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={openSuccess}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity="success"
+          sx={{ width: "100%", fontSize: 20 }}
+        >
+          Success
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={openError}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity="error"
+          sx={{ width: "100%", fontSize: 20 }}
+        >
+          {errorMessage}
+        </Alert>
+      </Snackbar>
       <div className={"photoHeader"}>
         <Battery percentage={"50%"} />
         <div className={"photoTitle"}>Last photo</div>
@@ -71,15 +98,22 @@ const LatestPhoto = ({ latestPhoto }) => {
           alt={"s"}
         />
       </div>
-      <div className={"dateText"}>{latestPhoto.timestamp ?
-          latestPhoto.timestamp.substring(0, 16) :
-          22
-      }</div>
+      <div className={"dateText"}>
+        {latestPhoto.timestamp ? latestPhoto.timestamp.substring(0, 16) : 22}
+      </div>
       <div className={"activeText"}>Active</div>
       <button
         className={isLoading ? "captureButtonDisabled" : "captureButton"}
         type="button"
-        onClick={() => handleClick(setLoading, setErrorMessage, setOpenError, setOpenSuccess)}
+        onClick={() =>
+          handleClick(
+            setLoading,
+            setErrorMessage,
+            setOpenError,
+            setOpenSuccess,
+            setLatestPhoto
+          )
+        }
         disabled={isLoading}
       >
         <div className={"cameraButtonRow"}>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,98 +1,123 @@
-import './Main.css';
+import "./Main.css";
 import LatestPhoto from "../components/Main/LatestPhoto.jsx";
 import Settings from "../components/Main/Settings.jsx";
 import * as React from "react";
-import { useEffect, useState } from 'react'
-import parameterService from '../services/parameters.js'
-import { Alert, Snackbar } from '@mui/material'
+import { useEffect, useState } from "react";
+import parameterService from "../services/parameters.js";
+import { Alert, Snackbar } from "@mui/material";
 function Main() {
+  const [saturation, setSaturation] = React.useState(0);
+  const [brightness, setBrightness] = React.useState(0);
+  const [contrast, setContrast] = React.useState(0);
+  const [cameraSettings, setCameraSettings] = React.useState({});
+  const [timeValues, setTimeValues] = React.useState([]);
+  const [openError, setOpenError] = useState(false);
+  const [latestPhoto, setLatestPhoto] = useState({});
+  const [errorMessage, setErrorMessage] = useState();
 
-    const [saturation, setSaturation] = React.useState(0);
-    const [brightness, setBrightness] = React.useState(0);
-    const [contrast, setContrast] = React.useState(0);
-    const [cameraSettings, setCameraSettings] = React.useState({})
-    const [timeValues, setTimeValues] = React.useState([])
-    const [openError, setOpenError] = useState(false);
-    const [latestPhoto, setLatestPhoto] = useState({});
-    const [errorMessage, setErrorMessage] = useState();
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`${parameterService.BASE_URL}/parameters`)
-                const data = await response.json()
-                console.log(data)
-                if (!response.ok) {
-                    setErrorMessage(`Error ${response.status}: Could not fetch the latest parameters from the backend.`)
-                    throw new Error(`${response.status} ${parameterService.getErrorMessage(response, data)}`);
-                }
-                setCameraSettings(data)
-                setBrightness(data.brightness)
-                setSaturation(data.saturation)
-                setContrast(data.contrast)
-                setTimeValues(data.schedule)
-            } catch (error) {
-                console.log(error)
-                setOpenError(true)
-            }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${parameterService.BASE_URL}/parameters`);
+        const data = await response.json();
+        console.log(data);
+        if (!response.ok) {
+          setErrorMessage(
+            `Error ${response.status}: Could not fetch the latest parameters from the backend.`
+          );
+          throw new Error(
+            `${response.status} ${parameterService.getErrorMessage(
+              response,
+              data
+            )}`
+          );
         }
-
-        const fetchLatestPhoto = async () => {
-            try {
-                const response = await fetch(`${parameterService.BASE_URL}/get_latest_photo`)
-                const data = await response.json()
-                console.log(data)
-                if (!response.ok) {
-                    setErrorMessage(`Error ${response.status}: Could not fetch the latest photo from the backend.`)
-                    throw new Error(`${response.status} ${parameterService.getErrorMessage(response, data)}`);
-                }
-                setLatestPhoto(data)
-            } catch (error) {
-                console.log(error)
-                setOpenError(true)
-            }
-        }
-
-        fetchLatestPhoto()
-            .catch(console.error)
-        fetchData()
-            .catch(console.error)
-    }, []);
-
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setOpenError(false)
+        setCameraSettings(data);
+        setBrightness(data.brightness);
+        setSaturation(data.saturation);
+        setContrast(data.contrast);
+        setTimeValues(data.schedule);
+      } catch (error) {
+        console.log(error);
+        setOpenError(true);
+      }
     };
 
-    return (
-        <div className="main-page">
-            <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={openError} autoHideDuration={6000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="error" sx={{ width: '100%', fontSize: 20 }}>
-                    {errorMessage}
-                </Alert>
-            </Snackbar>
-            <div className="left-container">
-                <LatestPhoto latestPhoto={latestPhoto}></LatestPhoto>
-            </div>
-            <div className="right-container">
-                <Settings
-                    cameraSettings = {cameraSettings}
-                    setCameraSettings = {setCameraSettings}
-                    saturation={saturation}
-                    setSaturation={setSaturation}
-                    brightness={brightness}
-                    setBrightness={setBrightness}
-                    contrast={contrast}
-                    setContrast={setContrast}
-                    timeValues={timeValues}
-                    setTimeValues={setTimeValues}
-                />
-            </div>
-        </div>
-    );
+    const fetchLatestPhoto = async () => {
+      try {
+        const response = await fetch(
+          `${parameterService.BASE_URL}/get_latest_photo`
+        );
+        const data = await response.json();
+        console.log(data);
+        if (!response.ok) {
+          setErrorMessage(
+            `Error ${response.status}: Could not fetch the latest photo from the backend.`
+          );
+          throw new Error(
+            `${response.status} ${parameterService.getErrorMessage(
+              response,
+              data
+            )}`
+          );
+        }
+        setLatestPhoto(data);
+      } catch (error) {
+        console.log(error);
+        setOpenError(true);
+      }
+    };
+
+    fetchLatestPhoto().catch(console.error);
+    fetchData().catch(console.error);
+  }, []);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenError(false);
+  };
+
+  return (
+    <div className="main-page">
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={openError}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity="error"
+          sx={{ width: "100%", fontSize: 20 }}
+        >
+          {errorMessage}
+        </Alert>
+      </Snackbar>
+      <div className="left-container">
+        <LatestPhoto
+          latestPhoto={latestPhoto}
+          setLatestPhoto={setLatestPhoto}
+        ></LatestPhoto>
+      </div>
+      <div className="right-container">
+        <Settings
+          cameraSettings={cameraSettings}
+          setCameraSettings={setCameraSettings}
+          saturation={saturation}
+          setSaturation={setSaturation}
+          brightness={brightness}
+          setBrightness={setBrightness}
+          contrast={contrast}
+          setContrast={setContrast}
+          timeValues={timeValues}
+          setTimeValues={setTimeValues}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Main;
